c4c594d71df4d0e6d31c7ff10a1bb686243945e2
fanc232CO <105767045+fanc232CO@users.noreply.github.com>
2022-06-24 21:57:10 +0800

    Add files via upload

--- iGOLD_pipline.sh
+++ iGOLD_pipline.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 fn_DEG=$1
 fn_SNP=$2
+#change by users:
+run_anaconda3= ~/anaconda3/bin/python3
+run_munge_sumstats= ~/softwares/ldsc/munge_sumstats.py
+run_ldsc= ~/softwares/ldsc/ldsc.py
+fn_snplist= ~/database_fan/1000G/w_hm3.snplist
+path_weights_hm3= ~/database_fan/1000G/1000G_Phase3_weights_hm3_no_MHC
+path_frq= ~/database_fan/1000G/1000G_Phase3_frq
+
+
 #enrich DEG and SNPs
-./enrich_DEG_SNP_forline.py $fn_DEG $fn_SNP
+$run_anaconda3 ./enrich_DEG_SNP_forline.py $fn_DEG $fn_SNP
 #LDSC enrich, generate sumstat
-python ~/softwares/ldsc/munge_sumstats.py \
+python $run_munge_sumstats \
     --sumstats ./input_sumstat.txt \
-    --merge-alleles ~/database_fan/1000G/w_hm3.snplist \
+    --merge-alleles $fn_snplist \
     --out input_sumstat \
     --a1-inc
 #calculate LDSC enrichment
-python ~/softwares/ldsc/ldsc.py \
+python $run_ldsc \
     --h2 input_sumstat.sumstats.gz \
     --intercept-h2 1 \
     --ref-ld-chr ./enrich_LDSC_lds/chr@ \
-    --w-ld-chr ~/database_fan/1000G/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC.\
+    --w-ld-chr $path_weights_hm3/weights.hm3_noMHC.\
     --overlap-annot \
-    --frqfile-chr ~/database_fan/1000G/1000G_Phase3_frq/1000G.EUR.QC.\
+    --frqfile-chr $path_frq/1000G.EUR.QC.\
     --out enrich_LDSC
 #summarize results
 ./summary_result.py
